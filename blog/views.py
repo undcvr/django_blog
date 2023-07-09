d645a7ec35c6cf4521470962aeff8bc16e4c153b
Diyor <diyor.iknu@gmail.com>
2023-07-09 20:14:32 +0500

    re-disigned; add log in method

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts, })
+    return render(request, 'blog/mainpage.html', {'posts': posts, })
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
